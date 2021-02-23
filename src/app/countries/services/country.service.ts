35ba4f0075655e156d6f63dbf116c0389a4433f4
Santigp258 <santigp258@gmail.com>
2021-02-23 16:05:38 -0500

    improve size petition

--- src/app/countries/services/country.service.ts
+++ src/app/countries/services/country.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
@@ -11,19 +11,26 @@ export class CountryService {
 
   constructor(private http: HttpClient) {}
 
+  get httpParams() {
+    return new HttpParams().set(
+      'fields',
+      'name;capital;flag;population;region;alpha2Code;'
+    ); //limit the search
+  }
+
   getCountryByRegion(region: string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.baseUrl}/region/${region}`);
+    const url = `${this.baseUrl}/region/${region}`;
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 
   getContryByName(name: string): Observable<Country[]> {
+    const url = `${this.baseUrl}/name/${name}`;
     return this.http
-      .get<Country[]>(`${this.baseUrl}/name/${name}`)
-      .pipe(
-        catchError(resp => of([]))
-      );
+      .get<Country[]>(url, { params: this.httpParams })
+      .pipe(catchError((resp) => of([])));
   }
 
-  getCountryByAlphaCode(code:string): Observable<Country>{
+  getCountryByAlphaCode(code: string): Observable<Country> {
     return this.http.get<Country>(`${this.baseUrl}/alpha/${code}`);
   }
 }
